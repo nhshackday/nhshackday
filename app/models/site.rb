a709210cd53df5676b628954880c55be59c34a06
Adam Challis <adam@liquidbronze.com>
2015-05-16 17:40:15 +0100

    Return as array

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,12 +1,7 @@
 class Site < ActiveRecord::Base
 
   def as_json(options = {})
-    output = {}
-    output[self.organisation_code] = {}
-    output[self.organisation_code][:name] = self.name
-    output[self.organisation_code][:address_line_1] = self.address_line_1
-    output[self.organisation_code][:address_line_4] = self.address_line_4
-    output
+    [self.organisation_code, self.name, self.address_line_1, self.address_line_4]
   end
 
 end
