96ddcd23cffb9faed7745ba6a4163731e5b9b241
Adam Challis <adam@liquidbronze.com>
2015-05-17 14:16:50 +0100

    UI From Kyle

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,7 +8,7 @@ myAppControllers.controller('FeedbackIndexController', function ($scope){
 
 });
 
-myAppControllers.controller('FeedbackValidationController', function ($scope, $rootScope, $location, $mdDialog, Hospital, Validation){
+myAppControllers.controller('FeedbackValidationController', function ($scope, $timeout, $rootScope, $location, $mdDialog, Hospital, Validation){
   $scope.gender_options = [
     {text: "Male", value: 1},
     {text: "Female", value: 2},
@@ -29,7 +29,7 @@ myAppControllers.controller('FeedbackValidationController', function ($scope, $r
   };
 
   $scope.hideRadios = function(hospital_id){
-    console.log(hospital_id);
+    var hospital_fields = $(".hospital[aria-checked=false]").hide();   
   };
 
   $scope.findHospital = function(formData){
@@ -49,10 +49,13 @@ myAppControllers.controller('FeedbackValidationController', function ($scope, $r
   };
 
   $scope.processForm = function(formData){
-    console.log(formData);
     if(formData){
       Validation.set(formData);
-      $location.path( "/feedback/form" ); 
+      $("#validation_dialog").removeClass("hide");
+      $("#validation_dialog").addClass("show_dialog");      
+      $timeout(function(){
+        $location.path("/feedback/form"); 
+      }, 5000);
     } else {
       $mdDialog.show(
         $mdDialog.alert()          
@@ -65,12 +68,38 @@ myAppControllers.controller('FeedbackValidationController', function ($scope, $r
   };  
 });
 
-myAppControllers.controller('FeedbackFormController', function ($scope, $mdDialog, $location, Form){
+myAppControllers.controller('FeedbackFormController', function ($scope, $timeout,  $anchorScroll, $mdDialog, $location, Form){
+  $scope.changeImage = function(){
+    console.log("in change image");
+    var value = $(".md-thumb-container")[0].style.cssText.replace("left: ","").replace("%;","");
+    if(value >= 75 && value <= 100){
+      console.log("happy");
+      $(".md-thumb").html("<img class='face' src='img/1_laugh.png'>");
+    } else if (value >= 55 && value <= 75){
+      $(".md-thumb").html("<img class='face' src='img/2_smile.png'>");
+    } else if (value >= 35 && value <= 54){
+      $(".md-thumb").html("<img class='face' src='img/3_neutral.png'>");
+    } else if (value >= 26 && value <= 34){
+      $(".md-thumb").html("<img class='face' src='img/4_sad.png'>");
+    } else if (value >= 0 && value <= 25){
+      $(".md-thumb").html("<img class='face' src='img/5_crying_20.png'>");
+    }
+  };
+  $scope.hideImage = function(){
+    $('.face').hide();
+  };
   $scope.processForm = function(formData){
     if(formData){
+      $location.hash('top_of_page');
+      // call $anchorScroll()
+      $anchorScroll();
       formData.reference = ("patient_ref" + (Math.random()*10000000)).replace(".","")
       Form.set(formData);
-      $location.path( "/feedback/complete" ); 
+      $("#form_dialog").removeClass("hide");
+      $("#form_dialog").addClass("show_dialog");      
+      $timeout(function(){
+        $location.path( "/feedback/complete" ); 
+      }, 2000);      
     } else {
       $mdDialog.show(
         $mdDialog.alert()          
@@ -83,9 +112,15 @@ myAppControllers.controller('FeedbackFormController', function ($scope, $mdDialo
   };  
 });
 
-myAppControllers.controller('FeedbackCompleteController', function ($scope, $location, Feedback){
+myAppControllers.controller('FeedbackCompleteController', function ($scope, $timeout,  $anchorScroll, $location, Feedback){
+  $location.hash('top_of_page');
+  // call $anchorScroll()
+  $anchorScroll();
   Feedback.send();
   $scope.reference = localStorage.reference;
+  $timeout(function(){
+    $location.path( "http://nhshackday.liquidbronze.com/patient_responses" ); 
+  }, 5000);      
   $scope.gotoIndex = function(){
     $location.path( "/feedback/index" );
   };
