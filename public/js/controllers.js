a2de4e26ac00d7bae8493376e1ccddb0c5685905
Adam Challis <adam@liquidbronze.com>
2015-05-17 11:53:58 +0100

    Latest changes

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,16 +10,21 @@ myAppControllers.controller('FeedbackIndexController', function ($scope){
 
 myAppControllers.controller('FeedbackValidationController', function ($scope, $rootScope, $location, $mdDialog, Hospital, Validation){
   $scope.gender_options = [
-    {text: "Male", value: 0},
-    {text: "Female", value: 1},
+    {text: "Male", value: 1},
+    {text: "Female", value: 2},
   ];
   $scope.stripPostcode = function(postcode){
-    if(postcode.length > 3){
-      var reg = /[a-zA-Z]{1,2}\d{1,2}\s?\d{1}(.*)/;
-      var new_pc = postcode.replace(postcode.match(reg)[1], "");
-      $rootScope.partial_postcode = new_pc;
-    }else{
-      $rootScope.partial_postcode = postcode;
+    console.log(postcode);
+    if(_.isUndefined(postcode)){
+      $rootScope.partial_postcode = "";
+    } else {
+      if(postcode.length > 3){
+        var reg = /[a-zA-Z]{1,2}\d{1,2}\s?\d{1}(.*)/;
+        var new_pc = postcode.replace(postcode.match(reg)[1], "");
+        $rootScope.partial_postcode = new_pc;
+      }else{
+        $rootScope.partial_postcode = "";
+      }
     }
   };
 
@@ -44,6 +49,7 @@ myAppControllers.controller('FeedbackValidationController', function ($scope, $r
   };
 
   $scope.processForm = function(formData){
+    console.log(formData);
     if(formData){
       Validation.set(formData);
       $location.path( "/feedback/form" ); 
