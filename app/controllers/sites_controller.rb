cca49e0868e12a65df40f36efe461e313d2d01f7
Adam Challis <adam@liquidbronze.com>
2015-05-17 12:20:44 +0100

    Restrict search to hospitals

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,7 @@ class SitesController < ApplicationController
 
   def search
     name = params[:name].to_s
-    sites = Site.where("name like ?", "%#{name}%").limit(12)
+    sites = Site.where("name like ?", "%#{name}% and name like '%hospital%'").limit(12)
     render :json => sites.to_json
   end
 
